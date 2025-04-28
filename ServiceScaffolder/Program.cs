885e7d4cf5f4f4424a7ea9a92c511595c1873c0f
sonkt <sonkt@outlook.com>
2025-04-28 22:04:43 +0700

    OK

---
+++ ServiceScaffolder/Program.cs
@@ -0,0 +1,15 @@
+﻿using Spectre.Console;
+using ServiceScaffolder.Services;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // CLI yêu cầu tên service từ người dùng
+        var serviceName = AnsiConsole.Ask<string>("Nhập [green]tên Service[/]:");
+
+        // Tạo Project mới dựa vào template
+        var generator = new ProjectGenerator();
+        await generator.GenerateServiceAsync(serviceName);
+    }
+}
